25740b885aafbb6612a244309b668bdf2bfd8344
Osama Asif <osama.asif20@gmail.com>
2022-02-16 20:36:18 +0500

    * API's calls updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,33 +6,16 @@ import 'package:sample_network_calls/constants/colors/color_constants.dart';
 import 'package:sample_network_calls/providers/sample_calls_providers.dart';
 import 'package:sample_network_calls/wrappers/user_list_wrapper.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  late SampleCallsProvider _callsProvider;
-  late UserListWrapper? _userListWrapper;
-  bool showLoader = true;
-  @override
-  void initState() {
-    _callsProvider = Provider.of<SampleCallsProvider>(context, listen: false);
-    _callsProvider.getUserList().then((value) {
-      _userListWrapper = value;
-      setState(() {
-        showLoader = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    double _height = MediaQuery.of(context).size.height;
-    double _width = MediaQuery.of(context).size.width;
+    final double _height = MediaQuery.of(context).size.height;
+    final double _width = MediaQuery.of(context).size.width;
+    final SampleCallsProvider _callsProvider =
+        Provider.of<SampleCallsProvider>(context, listen: false);
+    _callsProvider.getUserList();
     return Scaffold(
       backgroundColor: ColorConstants.white,
       appBar: AppBar(
@@ -45,22 +28,18 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Consumer<SampleCallsProvider>(
             child: SimpleLoaderComponent(),
             builder: (context, consumer, child) {
-              if (showLoader) {
+              if (consumer.showLoader) {
                 return child!;
-              } else if (_userListWrapper != null) {
+              } else if (consumer.userListWrapper != null) {
                 return RefreshIndicator(
                     onRefresh: () async {
-                      setState(() {
-                        showLoader = true;
-                      });
-                      _userListWrapper = await _callsProvider.getUserList();
-                      setState(() {
-                        showLoader = false;
-                      });
+                      consumer.changeLoaderState(true);
+                      await _callsProvider.getUserList();
                     },
                     child: ListView.builder(
                       itemBuilder: (context, index) {
-                        UserLisData user = _userListWrapper!.data![index];
+                        UserLisData user =
+                            consumer.userListWrapper!.data![index];
                         return ListTile(
                           title: Text(user.firstName!),
                           leading: ClipRRect(
@@ -70,19 +49,14 @@ class _HomeScreenState extends State<HomeScreen> {
                           subtitle: Text(user.email!),
                         );
                       },
-                      itemCount: _userListWrapper!.data!.length,
+                      itemCount: consumer.userListWrapper!.data!.length,
                     ));
               } else {
                 return ErrorComponent(
                   message: "${consumer.error}",
                   onTap: () async {
-                    setState(() {
-                      showLoader = true;
-                    });
-                    _userListWrapper = await _callsProvider.getUserList();
-                    setState(() {
-                      showLoader = false;
-                    });
+                    consumer.changeLoaderState(true);
+                    await _callsProvider.getUserList();
                   },
                 );
               }
